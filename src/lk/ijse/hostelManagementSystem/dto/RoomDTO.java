0c3315c7cea3c6f2cd8e4dbe8f51d3ed8b6e56ff
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-04-21 17:47:05 +0530

    dto

--- src/lk/ijse/hostelManagementSystem/dto/RoomDTO.java
+++ src/lk/ijse/hostelManagementSystem/dto/RoomDTO.java
@@ -1,4 +1,62 @@
 package lk.ijse.hostelManagementSystem.dto;
 
+import lk.ijse.hostelManagementSystem.entity.Student;
+
 public class RoomDTO {
+    private Student id;
+     private String type;
+    private String keyMoney;
+   private int qty;
+
+    public RoomDTO() {
+    }
+
+    public RoomDTO(Student id, String type, String keyMoney, int qty) {
+        this.id = id;
+        this.type = type;
+        this.keyMoney = keyMoney;
+        this.qty = qty;
+    }
+
+    public Student getId() {
+        return id;
+    }
+
+    public void setId(Student id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKeyMoney() {
+        return keyMoney;
+    }
+
+    public void setKeyMoney(String keyMoney) {
+        this.keyMoney = keyMoney;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomDTO{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", keyMoney='" + keyMoney + '\'' +
+                ", qty=" + qty +
+                '}';
+    }
 }
--- src/lk/ijse/hostelManagementSystem/dto/StudentDTO.java
+++ src/lk/ijse/hostelManagementSystem/dto/StudentDTO.java
@@ -1,4 +1,71 @@
 package lk.ijse.hostelManagementSystem.dto;
 
 public class StudentDTO {
+     private String id;
+     private String name;
+     private String address;
+     private String contactNo;
+     private String dob;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(String id, String name, String address, String contactNo, String dob) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contactNo = contactNo;
+        this.dob = dob;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contactNo='" + contactNo + '\'' +
+                ", dob='" + dob + '\'' +
+                '}';
+    }
 }
